cc2be6bf77b374ce231983534e1da52a8797ffc5
Sinping Tang <sinping1010@gmail.com>
2020-11-08 12:33:51 +0100

    console info update

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ var port = process.env.PORT || 5050;
     port: 3306, //access denied
 } */
 console.info("yahsin");
+console.info("Sinping");
 app.listen(port, function() {
     console.log("Node server is runing");
 });
