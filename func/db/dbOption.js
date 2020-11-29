c32ef29cb7525f216aceaf0e923f8b3b82f54a20
Kun-Yao <frankwang11@hotmail.com>
2020-11-29 12:41:32 +0100

    create db option modul

---
+++ func/db/dbOption.js
@@ -0,0 +1,11 @@
+var db_option = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'germany',
+    port: 3306
+}
+
+exports.db_option = function() {
+    return db_option;
+}
